0c4d98471949bd18d47990ab9dbbde7fc01fddf8
MrYious <rosariomark37@gmail.com>
2024-02-02 23:11:04 +0800

    feat: add About section

---
+++ src/app/About.tsx
@@ -0,0 +1,29 @@
+import profile from './_assets/images/profile.jpg'
+
+export default function About(props: { about: { header: string, body: string, footer: string}}){
+    return(
+        <section id='about' className="flex flex-col items-center bg-white py-7 gap-7">
+            <div className="text-2xl font-bold border-4 border-black px-4 py-2 rounded">
+                About Me
+            </div>
+            <div className="flex justify-center items-center gap-10">
+                <div>
+                    <img src={profile.src} className=' h-[350px] shadow-black' alt="profile" />
+                </div>
+                <div className=' border-black w-1/3 flex flex-col gap-3 text-md p-2 text-justify'>
+                    <div>
+                        {props.about.header}
+                    </div>
+                    <div className='border-t-2 border-black'></div>
+                    <div>
+                        {props.about.body}
+                    </div>
+                    <div className='border-t-2 border-black'></div>
+                    <div>
+                        {props.about.footer}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
