12afe664276e30f051ffe5e024ede9c1101031a7
MrYious <rosariomark37@gmail.com>
2024-02-04 19:39:16 +0800

    remove opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,22 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mark | Portfolio",
   description: "A personal portfolio website for Mark Edison Rosario, the Developer.",
-  keywords: ["Mark Edison Rosario", "Portfolio", "Developer"],
-  openGraph: {
-    title: 'Mark Rosario | Portfolio',
-    description: 'A personal portfolio website for Mark Edison Rosario, the Developer.',
-    url: 'https://markrosario.vercel.app/',
-    siteName: 'markrosario',
-    images: [
-      {
-        url: 'https://markrosario.vercel.app/_next/static/media/profile.8a944781.jpg', // Must be an absolute URL
-        width: 800,
-        height: 1300,
-      }
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
+  keywords: ["Mark Edison Rosario", "Portfolio", "Developer"]
 };
 
 export default function RootLayout({
