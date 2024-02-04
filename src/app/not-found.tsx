adcf8dc90e519a63a99914a30b693aedea49c64c
MrYious <rosariomark37@gmail.com>
2024-02-04 12:22:05 +0800

    replace ' with &apos;

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound(){
             <div className="text-7xl">404 Not Found</div>
             <div className="text-4xl font-semibold">Ackkkktually..... 🤓</div>
             <div className="flex flex-col w-1/3 gap-3">
-                <div className="">The page you're trying to find seems to have gone on an unexpected adventure and gotten lost in the digital wilderness.</div>
+                <div className="">The page you&apos;re trying to find seems to have gone on an unexpected adventure and gotten lost in the digital wilderness.</div>
                 <div className="">Want me to take you to the right place, Princess?</div>
             </div>
             <Link href="/" className="px-4 py-2 border-black border-2 rounded shadow-sm hover:shadow-black" >Take me Home</Link>
