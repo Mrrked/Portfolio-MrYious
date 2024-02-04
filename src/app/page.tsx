44a832830736b0b8bfcae2249b0e0b251366b65b
MrYious <rosariomark37@gmail.com>
2024-02-04 16:13:50 +0800

    feat: add vectors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,18 @@ import Project from "./Project"
 import Skill from "./Skill"
 import { Typewriter } from 'react-simple-typewriter'
 import avatar from './_assets/images/avatar.png'
+import vector1 from './_assets/images/vector_img/1.png'
+import vector2 from './_assets/images/vector_img/2.png'
+import vector3 from './_assets/images/vector_img/3.png'
+import vector4 from './_assets/images/vector_img/4.png'
+import vector5 from './_assets/images/vector_img/5.png'
 
 export default function Home() {
-  return (<>
+  return (<div  className=" ">
     <NavBar></NavBar>
+    <img src={vector3.src} alt="vector3" className="absolute -top-20 -left-20 "/>
     <main id='home' className="flex min-h-screen items-center bg-white text-black">
-      <div className="flex border-black  items-center justify-between w-full px-40 py-10 border-y-2 ">
+      <div className="flex border-black  items-center justify-between w-full px-40 py-10 border-y-8 border-b-[12px] ">
         <div className="flex flex-col gap-5">
           <div className=" text-5xl font-bold">{MyData.intro.name}</div>
           <div className=" text-2xl font-bold">{MyData.intro.title}</div>
@@ -51,10 +57,14 @@ export default function Home() {
         <img src={avatar.src} className=' h-[450px]' alt="avatar" />
       </div>
     </main>
+    <img src={vector1.src} alt="vector1" className="absolute -bottom-40 right-0"/>
     <About about={MyData.about}/>
+    <img src={vector2.src} alt="vector2" className="absolute -left-32"/>
     <Skill skills={MyData.skills}/>
+    <img src={vector4.src} alt="vector4" className="absolute right-0"/>
     <Project projects={MyData.projects}/>
+    <img src={vector5.src} alt="vector5" className="absolute -left-32"/>
     <Contact contact={MyData.contact}/>
     <Footer/>
-  </>);
+  </div>);
 }
